d8f5ac304964efb1712818d01bf1442e05d69a74
Shreyansh gohil <shreyanshgohil2510@gmail.com>
2023-07-04 17:52:35 +0530

    Pull from dev

--- src/constants/DATA.tsx
+++ src/constants/DATA.tsx
@@ -1,3 +1,10 @@
+import {
+  MailOutlined,
+  LockOutlined,
+  UserOutlined,
+  PhoneOutlined,
+} from '@ant-design/icons';
+
 export const userColumns = [
   {
     title: 'Organization Name',
@@ -90,3 +97,114 @@ export const userDrawerBody = {
 };
 
 export { userDataSource };
+
+export const registerFormData = [
+  {
+    title: 'First Name',
+    id: 'first_name',
+    type: 'text',
+    name: 'first_name',
+    defaultValue: '',
+    errorMessage: 'Please enter your first name',
+    placeholder: 'Enter your first name',
+    required: true,
+    // icon: 'UserOutlined',
+    icon: <UserOutlined className="register-icon" />,
+    rules: [
+      {
+        required: true,
+        message: 'Please input your first name!',
+      },
+    ],
+  },
+  {
+    title: 'Last Name',
+    id: 'last_name',
+    type: 'text',
+    name: 'last_name',
+    defaultValue: '',
+    errorMessage: 'Please enter your last name',
+    placeholder: 'Enter your Last Name',
+    required: true,
+    // icon: 'UserOutlined',
+    icon: <UserOutlined className="register-icon" />,
+    rules: [
+      {
+        required: true,
+        message: 'Please input your last name!',
+      },
+    ],
+  },
+  {
+    title: 'Email Address',
+    id: 'email',
+    type: 'text',
+    name: 'email',
+    defaultValue: '',
+    errorMessage: 'Please enter your email',
+    placeholder: 'Enter your email address',
+    required: true,
+    // icon: 'MailOutlined',
+    icon: <MailOutlined className="register-icon" />,
+    rules: [
+      {
+        required: true,
+        message: 'Please input your email address!',
+      },
+    ],
+  },
+  {
+    title: 'Phone Number',
+    id: 'phone',
+    type: 'text',
+    name: 'phone',
+    defaultValue: '',
+    errorMessage: 'Please input your phone number!',
+    placeholder: 'Enter your Phone Number',
+    required: true,
+    // icon: 'PhoneOutlined',
+    icon: <PhoneOutlined className="register-icon" />,
+    rules: [
+      {
+        required: true,
+        message: 'Please input your phone number!',
+      },
+    ],
+  },
+  {
+    title: 'Password',
+    id: 'password',
+    type: 'password',
+    name: 'password',
+    defaultValue: '',
+    errorMessage: 'Please input your password!',
+    placeholder: 'Enter your Password',
+    required: true,
+    // icon: 'LockOutlined',
+    icon: <LockOutlined className="register-icon" />,
+    rules: [
+      {
+        required: true,
+        message: 'Please input your password!',
+      },
+    ],
+  },
+  {
+    title: 'Re Enter Password',
+    id: 'confirm_password',
+    type: 'password',
+    name: 'confirm_password',
+    defaultValue: '',
+    errorMessage: 'Please input your password again!',
+    placeholder: 'Re Enter your Password',
+    required: true,
+    // icon: 'LockOutlined',
+    icon: <LockOutlined className="register-icon" />,
+    rules: [
+      {
+        required: true,
+        message: 'Please input your password again!',
+      },
+    ],
+  },
+];
